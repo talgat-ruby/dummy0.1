417c1393b4f402d869c46bf32f9bfb08bf0c411b
Talgat Sarybaev <talgat.s@protonmail.com>
2022-06-20 15:04:00 +0600

    Use fetchData func

--- src/data/data2.tsx
+++ src/data/data2.tsx
@@ -1,16 +1,9 @@
-const data = {
-  async function() {
-    let dataArray = [];
-    await fetch(
-      "https://finnhub.io/api/v1/stock/profile2?symbol=BDR&token=calf0eiad3ie9ojgktpg"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .then((data) => dataArray.push(data))
-      .then((dataArray) => {
-        return dataArray;
-      });
-  },
-};
+async function fetchData() {
+  const res = await fetch(
+    "https://finnhub.io/api/v1/stock/profile2?symbol=BDR&token=calf0eiad3ie9ojgktpg"
+  );
+  const data = await res.json();
+  return data
+}
 
-export default data;
+export default fetchData;
